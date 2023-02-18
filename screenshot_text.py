98311446460c1ed3d91129b01218c021d3d52f64
Overdjosed <114219289+Overdjosed@users.noreply.github.com>
2023-02-18 18:13:25 +0100

    Fixed error in path_to_Tesseract.py
    When the file existed, there was not a functionality to check if the path in existed, so if path_to_tesseract.txt was created, if tesseract path did not exist, script did not work.

--- screenshot_text.py
+++ screenshot_text.py
@@ -1,68 +1,104 @@
 import pytesseract
 from os import remove, getcwd, walk, path
-from os.path import exists
 from PIL import ImageGrab, Image
 import pyperclip
 import PySimpleGUI as sg
 import fnmatch
 
 
-def find(pattern, path = 'C:/'):
-    result = []
-    for root, dirs, files in walk(path):
-        for name in files:
-            if fnmatch.fnmatch(name, pattern):
-                result.append(path.join(root, name))
-    return result[0]
-
 class CText:
 
-    def find(pattern, path = 'C:/Program Files/'):
-        result = []
-        for root, dirs, files in walk(path):
+
+    def __init__(self) -> None:
+        '''
+        Initialize the CText Class, find the path and create the file where the path will be saved
+
+        Return None
+
+        '''
+        # Check out current working directory
+        self.__cwd = getcwd()
+        # Name the path where we will save the path of tesseract.exe
+        file_path = path.join(self.__cwd, 'path_to_tesseract.txt')
+
+        # Create the file
+        if not path.exists(file_path):
+            # Search path
+            tess_path = CText.find('tesseract.exe')
+            tess_path = tess_path.replace('\\', '/')
+            # Write Path
+            with open(file_path, 'w') as f:
+                f.write(tess_path)
+
+        # If file is created
+        else:
+
+            # Revise if path is in
+            with open(file_path, 'r') as f:
+                tess_path = f.readline().strip()
+
+            # If Path is not in, search it and save in it
+            if not tess_path:
+                tess_path = CText.find('tesseract.exe')
+                tess_path = tess_path.replace('\\', '/')
+                with open(file_path, 'w') as f:
+                    f.write(tess_path)
+        self.__tess_path : str = tess_path
+
+    @staticmethod
+    def find(pattern: str, path='C:/') -> str:
+        """
+        Finds the pattern of tesseract.
+
+        Return the pattern.
+        Args:
+            pattern (str): The pattern we are going to find
+            path (str): The path where we are going to start the map. Defaults to 'C:/'.
+
+        Raises:
+            ValueError: If there is no file tesseract.exe it raise error
+
+        Returns:
+            str: Path of tesseract.exe
+        """
+        for root, _, files in walk(path):
             for name in files:
                 if fnmatch.fnmatch(name, pattern):
-                    result.append(path.join(root, name))
-        return result
-
-    def __init__(self):
-        cwd = getcwd()
-        if not exists(cwd + '/path_to_tesseract.txt'):
-            self._file = open('path_to_tesseract.txt', 'w')
-            self._tess_path = find('tesseract.exe')
-            self._tess_path = self._tess_path.replace('\\', '/')
-            self._file.write(self._tess_path)
-            self._file.close()
-        else:
-            self._file = open('path_to_tesseract.txt', 'r')
-            self._tess_path = self._file.readlines(1)[0]
+                    return root + '/' + name
+        raise ValueError(f"No se encontró el archivo '{pattern}'")
 
 
     def _to_clipboard(self):
-        try:
-            im= ImageGrab.grabclipboard()
-            if isinstance(im, Image.Image):
+        '''
+        Method that instantly without Interface get the text of the image'''
 
-                wd = getcwd()
-                complete_path = wd + '/tmp.png'
-                im.save(complete_path)
+        # Get the clipboard
+        im= ImageGrab.grabclipboard()
+        if isinstance(im, Image.Image):
 
-                pytesseract.pytesseract.tesseract_cmd = self._tess_path
+            # Save image
+            complete_path = self.__cwd + '/tmp.png'
+            im.save(complete_path)
 
-                pyperclip.copy(pytesseract.image_to_string(Image.open(complete_path)))
+            pytesseract.pytesseract.tesseract_cmd = self.__tess_path
 
-                remove(complete_path)
+            # Send it to the model and put in the clippboard
+            pyperclip.copy(pytesseract.image_to_string(Image.open(complete_path)))
 
-        except:
-            layout = [[sg.Text('Debe copiar una captura de Pantalla')], [sg.Button('OK')]]
-            window = sg.Window('Información', layout)
-            while True:
-                event, values = window.read()
-                if event == 'OK' or event == sg.WIN_CLOSED:
-                    break
-            window.close()
+            # Removes image
+            remove(complete_path)
+            return
 
+        if path.exists(self.__cwd + '/tmp.png'):
+            remove(self.__cwd + '/tmp.png')
+        # Pop up an interface to show that user must use an screen capture
+        layout = [[sg.Text('Debe copiar una captura de Pantalla')], [sg.Button('OK')]]
+        window = sg.Window('Información', layout)
+        while True:
+            event, values = window.read()
+            if event == 'OK' or event == sg.WIN_CLOSED:
+                break
+        window.close()
 
-
-to_read = CText()
-to_read._to_clipboard()
+text = CText()
+text._to_clipboard()
